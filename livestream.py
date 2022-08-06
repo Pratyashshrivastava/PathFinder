5ffe08830b8349b7a07a3ad1e28a91fc0c2c43ea
ujwxl23 <ujwalkr23@gmail.com>
2022-08-06 13:53:46 +0530

    ..
    ..

--- livestream.py
+++ livestream.py
@@ -1,12 +1,12 @@
 import cv2
 
-capture=cv2.videoCapture("http://192.168.82.15:8080/video")
+capture=cv2.VideoCapture("http://192.168.82.15:8080/video")
 
 while(True):
     _, frame = capture.read()
     cv2.imshow('livestream', frame)
 
-    if cv2.waitkey(1) == ord("q"):
+    if cv2.waitKey(1) == ord("q"):
         break
 
 capture.release()
